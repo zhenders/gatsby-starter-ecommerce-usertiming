f447825eec4168a86e68d3bde1646aa1f5cd8833
zhenders <zacharyehenderson@gmail.com>
2020-03-27 13:10:04 -0400

    Create MeasureRender.js

---
+++ src/pages/MeasureRender.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class MeasureRender extends React.Component {
+  constructor() {
+    super();
+
+    this.mounted = false;
+  }
+
+  render() {
+    const name = this.props.name;
+    if (this.mounted) {
+      window.performance.mark(`${name}UpdateStart`);
+    } else {
+      window.performance.mark(`${name}MountStart`);
+    }
+    return this.props.children;
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    
+    const name = this.props.name;
+    window.performance.mark(`${name}MountEnd`);
+    window.performance.measure(`${name}Mount`, `${name}MountStart`, `${name}MountEnd`);
+  }
+
+  componentDidUpdate() {
+    const name = this.props.name;
+    window.performance.mark(`${name}UpdateEnd`);
+    window.performance.measure(`${name}Update`, `${name}UpdateStart`, `${name}UpdateEnd`);
+  }
+}
+
